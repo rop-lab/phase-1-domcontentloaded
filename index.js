ee34c05a82f62c19e44bcaedd7e8593847afcf37
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-15 09:55:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    // Target the paragraph element with id="text"
+    var textParagraph = document.getElementById('text');
+  
+    // Replace the text content with "This is really cool!"
+    textParagraph.textContent = "This is really cool!";
+  });
+  
